d8680f9c846b369aa47340d364f7dc8bc021754c
rejoanahmed <87889021+rejoanahmed@users.noreply.github.com>
2022-07-15 07:51:28 +0600

    promise

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-function getUsers(callback) {
-  setTimeout(() => {
-    callback([
-      { username: "john", email: "john@test.com" },
-      { username: "jane", email: "jane@test.com" },
-    ]);
-  }, 1000);
-}
-const finduser = (callback) => {
-  getUsers((users) => {
-    const res = users.find(({ username, email }) => {
-      return username === "john";
-    });
-    callback(res);
+const getUsers = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res([
+        { username: "john", email: "john@test.com" },
+        { username: "jane", email: "jane@test.com" },
+      ]);
+    }, 1000);
   });
 };
-
-// function findUser(callback) {
-//   getUsers((users) => {
-//     const user = users.find(({ username, email }) => username === "john");
-//     callback(user);
-//   });
-// }
-// console.log("first");
-finduser(console.log);
-// console.log(getUsers().find(({ username, email }) => username === "john"));
+getUsers().then((users) => {
+  const john = users.find(({ username, email }) => username === "john");
+  console.log(john);
+});
