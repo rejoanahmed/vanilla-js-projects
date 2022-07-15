54f875761d6a2a4f06597292dc5868c028be4069
rejoanahmed <87889021+rejoanahmed@users.noreply.github.com>
2022-07-15 07:38:26 +0600

    bra

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ function getUsers(callback) {
 const finduser = (callback) => {
   getUsers((users) => {
     const res = users.find(({ username, email }) => {
-      username === "john";
+      return username === "john";
     });
     callback(res);
   });
 };
 
+// function findUser(callback) {
+//   getUsers((users) => {
+//     const user = users.find(({ username, email }) => username === "john");
+//     callback(user);
+//   });
+// }
 // console.log("first");
 finduser(console.log);
 // console.log(getUsers().find(({ username, email }) => username === "john"));
